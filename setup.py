888c040ff12adb5fb6d43e6507957d0b6087ff93
Joe Wallwork <joe.wallwork@outlook.com>
2023-08-04 07:24:20 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="animate",
+    description="An Anisotropic Mesh Adaptation Toolkit for Firedrake",
+    author="Joseph Wallwork",
+    author_email="joe.wallwork@outlook.com",
+    url="https://github.com/pyroteus/animate",
+    version="0.1",
+    packages=["animate"],
+)
