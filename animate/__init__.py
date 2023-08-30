51026efdb21614b664a0d230ca63ff8a8fea0b37
Joe Wallwork <joe.wallwork@outlook.com>
2023-08-30 08:39:29 +0100

    Add math and recovery to star imports

--- animate/__init__.py
+++ animate/__init__.py
@@ -1,4 +1,6 @@
 from animate.adapt import *  # noqa
 from animate.interpolation import *  # noqa
+from animate.math import *  # noqa
 from animate.metric import *  # noqa
+from animate.recovery import *  # noqa
 from animate.quality import *  # noqa
