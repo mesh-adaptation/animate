b1a60938539f13254489fbeb9256b459d20c851c
Joe Wallwork <joe.wallwork@outlook.com>
2023-09-23 08:23:09 +0100

    Minor tweaks to install script for clarity

--- install/install_firedrake.sh
+++ install/install_firedrake.sh
@@ -3,21 +3,22 @@
 # ====================================================================== #
 # Bash script for installing Firedrake based on a PETSc installation     #
 # which uses Mmg and ParMmg.                                             #
-#                                                                        #
-# Note that we use a custom PETSc branch.                                #
 # ====================================================================== #
 
-# Unset PYTHONPATH and PETSc env variables
-PYTHONPATH_TMP=${PYTHONPATH}
+# *** YOU MAY WISH TO EDIT THESE ENVIRONMENT VARIABLES: ***
+
+FIREDRAKE_ENV=firedrake-sep23
+PETSC_BRANCH=jwallwork23/parmmg-rebased
+
+# *** YOU SHOULD NOT NEED TO EDIT ANYTHING BELOW. ***
+
+# Environment variables for Firedrake installation
+CWD=$(pwd)
 unset PYTHONPATH
 unset PETSC_DIR
 unset PETSC_ARCH
-
-# Environment variables for Firedrake installation
-FIREDRAKE_ENV=firedrake-adapt
-FIREDRAKE_DIR=${SOFTWARE}/${FIREDRAKE_ENV}
-PETSC_BRANCH=jwallwork23/parmmg-rebased 
 export PETSC_CONFIGURE_OPTIONS="$(cat petsc_options.txt | tr '\n' ' ')"
+FIREDRAKE_DIR=${SOFTWARE}/${FIREDRAKE_ENV}
 
 # Check environment variables
 echo "FIREDRAKE_ENV=${FIREDRAKE_ENV}"
@@ -25,17 +26,13 @@ echo "FIREDRAKE_DIR=${FIREDRAKE_DIR}"
 echo "PETSC_BRANCH=${PETSC_BRANCH}"
 echo "PETSC_CONFIGURE_OPTIONS=${PETSC_CONFIGURE_OPTIONS}"
 echo "python3=$(which python3)"
-echo "Are these settings okay? Press enter to continue."
+echo "Are these settings okay? Press any key to continue or Ctrl+C to exit."
 read chk
 
-# Install Firedrake
+# Install Firedrake using the above configuration
 curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install
 python3 firedrake-install --venv-name ${FIREDRAKE_ENV} --package-branch petsc ${PETSC_BRANCH}
 source ${FIREDRAKE_DIR}/bin/activate
-
-# Reset environment
-export PYTHONPATH=${PYTHONPATH_TMP}
-unset PYTHONPATH_TMP
 unset PETSC_CONFIGURE_OPTIONS
 
 # Install Animate and test that the adaptation functionality is working
@@ -44,3 +41,5 @@ git clone git@github.com:pyroteus/animate
 cd animate
 make install
 make test
+
+cd ${CWD}
