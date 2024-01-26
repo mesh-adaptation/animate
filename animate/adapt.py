88fda36610afecf77ab14e25f92cc4f81380fba1
Joe Wallwork <joe.wallwork@outlook.com>
2024-01-26 07:52:02 +0000

    #71: First draft adapt subproc

--- animate/adapt.py
+++ animate/adapt.py
@@ -5,7 +5,11 @@ import firedrake.mesh as fmesh
 from firedrake.petsc import PETSc
 from firedrake.projection import Projector
 import firedrake.utils as futils
+from firedrake import CheckpointFile, COMM_WORLD
 from .metric import RiemannianMetric
+from .utility import get_venv_path
+import os
+import subprocess
 
 __all__ = ["MetricBasedAdaptor", "adapt"]
 
@@ -119,7 +123,7 @@ class MetricBasedAdaptor(AdaptorBase):
         )  # TODO
 
 
-def adapt(mesh, *metrics, name=None):
+def adapt(mesh, *metrics, name=None, serial=False, remove_checkpoints=True):
     r"""
     Adapt a mesh with respect to a metric and some adaptor parameters.
 
@@ -128,10 +132,75 @@ def adapt(mesh, *metrics, name=None):
     :param mesh: :class:`~firedrake.mesh.MeshGeometry` to be adapted.
     :param metrics: list of :class:`.RiemannianMetric`\s
     :param name: name for the adapted mesh
+    :param serial: should the adaptation be done in serial?
+    :param remove_checkpoints: should checkpoint files be deleted after use?
     :return: a new :class:`~firedrake.mesh.MeshGeometry`.
     """
+
+    # Parallel adaptation is currently only supported in 3D
+    if mesh.topological_dimension() != 3:
+        serial = True
+
+    # If already running in serial then no need to use checkpointing
+    if COMM_WORLD.size == 1:
+        serial = False
+
+    # Combine metrics by intersection, if multiple are passed
     metric = metrics[0]
     if len(metrics) > 1:
         metric.intersect(*metrics[1:])
-    adaptor = MetricBasedAdaptor(mesh, metric, name=name)
-    return adaptor.adapted_mesh
+
+    if serial:
+        fpath = os.path.join(get_venv_path(), "src", "animate", ".checkpoints")
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)
+
+        # In parallel, save input mesh and metric to a checkpoint file
+        input_fname = os.path.join(fpath, "tmp_metric.h5")
+        with CheckpointFile(input_fname, "w") as chk:
+            chk.save_mesh(mesh, name="tmp_input_mesh")
+            chk.save_function(metric, name="tmp_metric")
+
+        # In serial, load the checkpoint, adapt and write out the result
+        if COMM_WORLD.rank == 0:
+            adapt_script = os.path.join(fpath, "src", "animate", "animate", "adapt.py")
+            subprocess.run(["mpiexec", "-n", "1", "python3", adapt_script])
+        COMM_WORLD.barrier()
+
+        # In parallel, load from the checkpoint
+        output_fname = os.path.join(fpath, "tmp_mesh.h5")
+        if not os.path.exists(output_fname):
+            raise Exception(f"Adapted mesh file does not exist! Path: {output_fname}.")
+        with CheckpointFile(output_fname, "r") as chk:
+            newmesh = chk.load_mesh("tmp_adapted_mesh")
+
+        # Delete temporary checkpoint files
+        if remove_checkpoints:
+            os.remove(input_fname)
+            os.remove(output_fname)
+    else:
+        adaptor = MetricBasedAdaptor(mesh, metric, name=name)
+        newmesh = adaptor.adapted_mesh
+    return newmesh
+
+
+if __name__ == "__main__":
+    fpath = os.path.join(get_venv_path(), "src", "animate", ".checkpoints")
+    assert os.path.exists(fpath)
+    input_fname = os.path.join(fpath, "tmp_metric.h5")
+    if not os.path.exists(input_fname):
+        raise Exception(f"Metric file does not exist! Path: {input_fname}.")
+
+    # Load input mesh and metric from checkpoint
+    with CheckpointFile(input_fname, "r") as chk:
+        mesh = chk.load_mesh("tmp_input_mesh")
+        metric = chk.load_function("tmp_metric")
+
+    # Convert metric from Function to RiemannianMetric, then adapt
+    metric = RiemannianMetric(metric.function_space()).assign(metric)
+    adaptor = MetricBasedAdaptor(mesh, metric, name="tmp_adapted_mesh")
+
+    # Write adapted mesh to another checkpoint
+    output_fname = os.path.join(fpath, "tmp_mesh.h5")
+    with CheckpointFile(output_fname, "w") as chk:
+        chk.save_mesh(adaptor.adapted_mesh)
