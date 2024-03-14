47ab149fdb42444260ace14037b2484d9921db1a
Joe Wallwork <joe.wallwork@outlook.com>
2024-03-14 17:55:42 +0000

    Revert to b97ba84

--- animate/adapt.py
+++ animate/adapt.py
@@ -200,24 +200,29 @@ def adapt(mesh, *metrics, name=None, serialise=False, remove_checkpoints=True):
 
         # In serial, load the checkpoint, adapt and write out the result
         PETSc.Sys.Print("Processor %d" % COMM_WORLD.rank, comm=COMM_SELF)
+        # saved = [False] * COMM_WORLD.size
         if COMM_WORLD.rank == 0:
             PETSc.Sys.Print("Processor %d: DEBUG 1" % COMM_WORLD.rank, comm=COMM_SELF)
+            PETSc.Sys.Print("Processor %d: DEBUG 2" % COMM_WORLD.rank, comm=COMM_SELF)
             metric0 = load_checkpoint(
                 metric_fname, mesh.name, metric_name, comm=COMM_SELF
             )
-            PETSc.Sys.Print("Processor %d: DEBUG 2" % COMM_WORLD.rank, comm=COMM_SELF)
-            adaptor0 = MetricBasedAdaptor(metric0._mesh, metric0, name=name)
             PETSc.Sys.Print("Processor %d: DEBUG 3" % COMM_WORLD.rank, comm=COMM_SELF)
+            adaptor0 = MetricBasedAdaptor(metric0._mesh, metric0, name=name)
+            PETSc.Sys.Print("Processor %d: DEBUG 4" % COMM_WORLD.rank, comm=COMM_SELF)
             with fchk.CheckpointFile(output_fname, "w", comm=COMM_SELF) as chk:
                 PETSc.Sys.Print(
-                    "Processor %d: DEBUG 4" % COMM_WORLD.rank, comm=COMM_SELF
+                    "Processor %d: DEBUG 5" % COMM_WORLD.rank, comm=COMM_SELF
                 )
                 chk.save_mesh(adaptor0.adapted_mesh)
-            PETSc.Sys.Print("Processor %d: DEBUG 5" % COMM_WORLD.rank, comm=COMM_SELF)
+            PETSc.Sys.Print("Processor %d: DEBUG 6" % COMM_WORLD.rank, comm=COMM_SELF)
+            # saved[0] = True
+            PETSc.Sys.Print("Processor %d: DEBUG 7" % COMM_WORLD.rank, comm=COMM_SELF)
         else:
-            # TODO: Why do we need to have the other processors do something?
             time.sleep(1e-5)
         COMM_WORLD.barrier()
+        # if not COMM_WORLD.scatter(saved, root=0):
+        #     raise Exception
 
         # In parallel, load from the checkpoint
         if not os.path.exists(output_fname):
