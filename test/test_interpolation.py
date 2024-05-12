3cd9d841d8b7f397f28f652f9d39dfa8c0c00d29
Joe Wallwork <joe.wallwork@outlook.com>
2024-05-12 16:15:05 +0100

    #95: Increase mesh resolutions for TestTransfer

--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -131,8 +131,8 @@ class TestTransfer(unittest.TestCase):
     """
 
     def setUp(self):
-        self.source_mesh = UnitSquareMesh(2, 2, diagonal="left")
-        self.target_mesh = UnitSquareMesh(2, 3, diagonal="right")
+        self.source_mesh = UnitSquareMesh(4, 4, diagonal="left")
+        self.target_mesh = UnitSquareMesh(4, 5, diagonal="right")
 
     def sinusoid(self, source=True):
         x, y = SpatialCoordinate(self.source_mesh if source else self.target_mesh)
