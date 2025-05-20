ff11c5c386a11ecbd49138973a180c49ee33dc4b
Joe Wallwork <jw2423@cam.ac.uk>
2025-05-20 07:38:19 +0100

    tmp

--- demos/metric_normalisation.py
+++ demos/metric_normalisation.py
@@ -16,6 +16,7 @@
 # A naive approach is to rescale as
 #
 # ..math::
+#     :label:`l_infty`
 #
 #     \widetilde{\mathcal{M}}=
 #     \frac{\mathcal{C}_T}{\mathcal{C}(\mathcal{M})}\:\mathcal{M},
@@ -27,4 +28,34 @@
 #     \mathcal{C}(\mathcal{M})=\int_\Omega\det(M(x))\,\mathrm{d}x
 #
 # is the complexity of :math:`\mathcal{M}` and :math:`\mathcal{C}_T` is the target
-# complexity.
+# complexity. This is actually a special case of the more general :math:`L^p`
+# normalisation approach (the infinite limit :math:`p\rightarrow\infty`. Before digging
+# further into :math:`L^p` normalisation, let's test out the so-called :math:`L^\infty`
+# approach described above.
+#
+# For testing purposes, consider a hyperbolic sensor function defined on the square
+# domain :math:`[-1,1]^2`. The sensor functions used was defined in
+# :cite:`Olivier:2011` and is interesting because it contains background oscillations,
+# in addition to larger-scale features. ::
+
+from firedrake import *
+from animate import *
+
+
+def hyperbolic(x, y):
+    sn = sin(50 * x * y)
+    return conditional(abs(x * y) < 2 * pi / 50, 0.01 * sn, sn)
+
+
+# Define a square mesh on :math:`[0,2]^2` and subtract 1 from each coordinate to get a
+# mesh of the desired domain. ::
+
+mesh = SquareMesh(20, 20, 2, 2)
+mesh.coordinates[:] -= 1
+mesh = Mesh(mesh.coordinates)
+
+# TODO: Plot sensor function
+# TODO: Adapt with L^infty normalisation
+# TODO: Describe L^p normalisation
+# TODO: Adapt with L^p normalisation for p=1,2,4
+# TODO: Add Olivier citation
