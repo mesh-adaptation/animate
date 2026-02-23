2370f2cf7e63662d47f5d74289f735db99952009
Joe Wallwork <jw2423@cam.ac.uk>
2026-02-23 17:41:58 +0000

    Account for different ranks in unit test

--- test/test_numbering.py
+++ test/test_numbering.py
@@ -7,15 +7,33 @@ from animate.cython.numbering import to_petsc_local_numbering
 
 
 @pytest.fixture
-def setup_function_space():
-    """Fixture to create a Firedrake FunctionSpace."""
-    return fd.FunctionSpace(fd.UnitSquareMesh(4, 4), "CG", 1)
+def mesh():
+    """Fixture to create a uniform unit square mesh."""
+    return fd.UnitSquareMesh(4, 4)
 
 
-def test_is_permutation(setup_function_space):
+@pytest.fixture(params=[0, 1, 2])
+def rank(request):
+    """Fixture specifying function space rank."""
+    return request.param
+
+
+@pytest.fixture
+def function_space(mesh, rank):
+    """Fixture to create a function space on a given mesh with a given rank."""
+    try:
+        return {
+            0: fd.FunctionSpace,
+            1: fd.VectorFunctionSpace,
+            2: fd.TensorFunctionSpace,
+        }[rank](mesh, "CG", 1)
+    except KeyError as key_err:
+        raise ValueError(f"Rank {rank} not considered.") from key_err
+
+
+def test_is_permutation(function_space):
     """Test that to_petsc_local_numbering is indeed a permutation."""
-    V = setup_function_space
-    f = fd.Function(V)
+    f = fd.Function(function_space)
 
     # Fill the vector with arbitrary values
     with f.dat.vec as vec:
@@ -23,7 +41,7 @@ def test_is_permutation(setup_function_space):
 
     # Verify that reordering according to the PETSc numbering is a permutation
     with f.dat.vec_ro as vec:
-        reordered_vec = to_petsc_local_numbering(vec, V)
+        reordered_vec = to_petsc_local_numbering(vec, function_space)
         assert reordered_vec is not None
         assert reordered_vec.size == vec.size
         assert sorted(reordered_vec.array) == sorted(vec.array)
