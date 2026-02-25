49a5595766d6fbac143f501cb5536f22e4cb2d03
Stephan Kramer <s.kramer@imperial.ac.uk>
2026-02-25 11:23:49 +0000

    Extend numbering test to parallel

--- test/test_numbering.py
+++ test/test_numbering.py
@@ -4,6 +4,7 @@ import firedrake as fd
 import numpy as np
 import pytest
 from animate.cython.numbering import to_petsc_local_numbering
+from firedrake.petsc import PETSc
 
 
 @pytest.fixture
@@ -35,13 +36,43 @@ def test_is_permutation(function_space):
     """Test that to_petsc_local_numbering is indeed a permutation."""
     f = fd.Function(function_space)
 
+    comm = function_space.comm
+    # unique float 0<=x<1 for each rank:
+    rank_fraction = comm.rank / (comm.size + 1)
+
+    f.assign(-1)
+
     # Fill the vector with arbitrary values
+    # vec below is a global vec, so owned entries only
+    # halos values should be updated automatically coming out of the context
     with f.dat.vec as vec:
-        vec.array[:] = np.arange(vec.size)
+        vec.array[:] = np.arange(vec.sizes[0]) + rank_fraction
+
+    # Check that this is the case:
+    owned = f.dat.data_ro.flatten()
+    halos = f.dat.data_ro_with_halos[owned.size:].flatten()
+    np.testing.assert_equal(owned, np.arange(owned.size) + rank_fraction)
+    if halos.size > 0:
+        # all halo enties should be set and come from different rank
+        assert all(halos >= 0.0)
+        assert all(np.modf(halos)[0] != rank_fraction)
 
     # Verify that reordering according to the PETSc numbering is a permutation
-    with f.dat.vec_ro as vec:
-        reordered_vec = to_petsc_local_numbering(vec, function_space)
-        assert reordered_vec is not None
-        assert reordered_vec.size == vec.size
-        assert sorted(reordered_vec.array) == sorted(vec.array)
+    data = f.dat.data_ro_with_halos
+    lvec = PETSc.Vec().createWithArray(
+        data, size=data.size, bsize=f.dat.cdim, comm=fd.COMM_SELF
+    )
+    reordered_vec = to_petsc_local_numbering(lvec, function_space)
+    assert reordered_vec is not None
+    assert reordered_vec.size == lvec.size
+    assert sorted(reordered_vec.array) == sorted(lvec.array)
+
+
+@pytest.mark.parallel(nprocs=2)
+def test_is_permutation_np2(function_space):
+    test_is_permutation(function_space)
+
+
+@pytest.mark.parallel(nprocs=3)
+def test_is_permutation_np3(function_space):
+    test_is_permutation(function_space)
